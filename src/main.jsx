f2f03f66657f6c8d46b093c84ee673d0b24bf39c
md-masum <masum.nextu@gmail.com>
2023-05-02 01:34:09 +0600

    created header with bacground img

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Header from './components/Header/Header.jsx';
+import Login from './components/login/login/Login.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header></Header>
+  },
+  {
+    path:'/login',
+    element:<Login></Login>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+        <RouterProvider router={router} />
   </React.StrictMode>,
 )
