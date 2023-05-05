94a9221778c0de7534a686446e00cd64ab46d1e1
md-masum <masum.nextu@gmail.com>
2023-05-05 23:08:32 +0600

    footer and some section updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,16 +5,16 @@ const Footer = () => {
         <footer className="footer p-10 bg-base-300 text-base-content">
         <div>
           <span className="footer-title">Services</span> 
-          <a className="link link-hover">Branding</a> 
-          <a className="link link-hover">Design</a> 
-          <a className="link link-hover">Marketing</a> 
-          <a className="link link-hover">Advertisement</a>
+          <a className="link link-hover">Rosmalai</a> 
+          <a className="link link-hover">Beriani</a> 
+          <a className="link link-hover">Shotki Borta</a> 
+          <a className="link link-hover">Kachori</a>
         </div> 
         <div>
           <span className="footer-title">Company</span> 
           <a className="link link-hover">About us</a> 
           <a className="link link-hover">Contact</a> 
-          <a className="link link-hover">Jobs</a> 
+          <a className="link link-hover">Blogs</a> 
           <a className="link link-hover">Press kit</a>
         </div> 
         <div>
